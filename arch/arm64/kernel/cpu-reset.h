abf369e9b0f5c8bd0d429e7c2b9a87f5765c3ccd
cwd.systems <101006007+infinitydaemon@users.noreply.github.com>
2023-04-24 13:40:53 +0500

    Update cpu-reset.h

--- arch/arm64/kernel/cpu-reset.h
+++ arch/arm64/kernel/cpu-reset.h
@@ -11,18 +11,17 @@
 #include <asm/virt.h>
 
 void __cpu_soft_restart(unsigned long el2_switch, unsigned long entry,
-	unsigned long arg0, unsigned long arg1, unsigned long arg2);
+			 unsigned long arg0, unsigned long arg1, unsigned long arg2);
 
 static inline void __noreturn __nocfi cpu_soft_restart(unsigned long entry,
 						       unsigned long arg0,
 						       unsigned long arg1,
 						       unsigned long arg2)
 {
-	typeof(__cpu_soft_restart) *restart;
-
-	unsigned long el2_switch = !is_kernel_in_hyp_mode() &&
-		is_hyp_mode_available();
-	restart = (void *)__pa_symbol(function_nocfi(__cpu_soft_restart));
+	bool el2_switch = !is_kernel_in_hyp_mode() && is_hyp_mode_available();
+	void (*restart)(unsigned long, unsigned long, unsigned long,
+			unsigned long, unsigned long) =
+		(void *)__pa_symbol(function_nocfi(__cpu_soft_restart));
 
 	cpu_install_idmap();
 	restart(el2_switch, entry, arg0, arg1, arg2);
