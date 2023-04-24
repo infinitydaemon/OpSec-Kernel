fd01b9c0150579578b77f1ebc81b7c3e4c500b2b
cwd.systems <101006007+infinitydaemon@users.noreply.github.com>
2023-04-24 13:33:39 +0500

    Update kaslr_early.c

--- arch/arm64/kernel/pi/kaslr_early.c
+++ arch/arm64/kernel/pi/kaslr_early.c
@@ -85,20 +85,18 @@ static u64 get_kaslr_seed(void *fdt)
 	return ret;
 }
 
-asmlinkage u64 kaslr_early_init(void *fdt)
+asmlinkage u64 kaslr_get_seed(void *fdt)
 {
-	u64 seed;
+	u64 kaslr_seed;
 
-	if (is_kaslr_disabled_cmdline(fdt))
-		return 0;
-
-	seed = get_kaslr_seed(fdt);
-	if (!seed) {
-		if (!__early_cpu_has_rndr() ||
-		    !__arm64_rndr((unsigned long *)&seed))
+	kaslr_seed = get_kaslr_seed(fdt);
+	if (!kaslr_seed) {
+		if (!__early_cpu_has_rndr() || !__arm64_rndr((unsigned long *)&kaslr_seed))
 			return 0;
 	}
 
+	return kaslr_seed;
+}
 	/*
 	 * OK, so we are proceeding with KASLR enabled. Calculate a suitable
 	 * kernel image offset from the seed. Let's place the kernel in the
